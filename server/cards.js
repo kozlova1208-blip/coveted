1bee4a6b2dd68355606c49141cda0a154bb01a7d
kozzni <kozlova1208@gmail.com>
2026-05-05 22:24:54 +0100

    Add first 2 real NAP cards

--- server/cards.js
+++ server/cards.js
@@ -3,13 +3,18 @@
 // image = right-click any NAP product photo → "Copy image address"
 
 const cards = [
-  // Paste your cards here, e.g.:
-  // {
-  //   id: '001',
-  //   brand: 'Loewe',
-  //   name: 'Puzzle Bag',
-  //   image: 'https://www.net-a-porter.com/variants/images/46376663163091741/in/w920_q60.jpg',
-  // },
+  {
+    id: '001',
+    brand: 'Jenny Packham',
+    name: 'Gown',
+    image: 'https://www.net-a-porter.com/variants/images/46376663163079791/ou/w2000_q60.jpg',
+  },
+  {
+    id: '002',
+    brand: 'Versace',
+    name: 'Shirt',
+    image: 'https://www.net-a-porter.com/variants/images/46376663163047532/ou/w2000_q60.jpg',
+  },
 ];
 
 module.exports = { cards };
