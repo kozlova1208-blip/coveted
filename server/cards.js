d653e294d158594f83f5235ca1e301304dc681b1
kozzni <kozlova1208@gmail.com>
2026-05-05 22:47:04 +0100

    Add 15 real NAP cards

--- server/cards.js
+++ server/cards.js
@@ -1,6 +1,5 @@
 // Luxit card deck
 // Each card needs only: id, brand, name, image
-// image = right-click any NAP product photo → "Copy image address"
 
 const cards = [
   {
@@ -14,6 +13,84 @@ const cards = [
     brand: 'Versace',
     name: 'Shirt',
     image: 'https://www.net-a-porter.com/variants/images/46376663163047532/ou/w2000_q60.jpg',
+  },
+  {
+    id: '003',
+    brand: 'Lanvin',
+    name: 'Dress',
+    image: 'https://www.net-a-porter.com/variants/images/46376663162958032/ou/w2000_q60.jpg',
+  },
+  {
+    id: '004',
+    brand: 'Bode',
+    name: 'Dress',
+    image: 'https://www.net-a-porter.com/variants/images/46376663162967268/ou/w2000_q60.jpg',
+  },
+  {
+    id: '005',
+    brand: 'Acne Studios',
+    name: 'Jacket',
+    image: 'https://www.net-a-porter.com/variants/images/46376663162964233/ou/w2000_q60.jpg',
+  },
+  {
+    id: '006',
+    brand: 'Staud',
+    name: 'Bag',
+    image: 'https://www.net-a-porter.com/variants/images/46376663162986142/ou/w2000_q60.jpg',
+  },
+  {
+    id: '007',
+    brand: 'JW Anderson',
+    name: 'Bag',
+    image: 'https://www.net-a-porter.com/variants/images/46376663162971353/ou/w2000_q60.jpg',
+  },
+  {
+    id: '008',
+    brand: 'Chloe',
+    name: 'Jacket',
+    image: 'https://www.net-a-porter.com/variants/images/46376663162848011/ou/w2000_q60.jpg',
+  },
+  {
+    id: '009',
+    brand: 'Farm Rio',
+    name: 'Shorts',
+    image: 'https://www.net-a-porter.com/variants/images/46376663162892034/ou/w2000_q60.jpg',
+  },
+  {
+    id: '010',
+    brand: 'Alemais',
+    name: 'Trousers',
+    image: 'https://www.net-a-porter.com/variants/images/46376663162877471/ou/w2000_q60.jpg',
+  },
+  {
+    id: '011',
+    brand: 'Khaite',
+    name: 'Skirt',
+    image: 'https://www.net-a-porter.com/variants/images/1647597358475253/ou/w2000_q60.jpg',
+  },
+  {
+    id: '012',
+    brand: 'Dries Van Noten',
+    name: 'Pants',
+    image: 'https://www.net-a-porter.com/variants/images/46376663162929264/ou/w2000_q60.jpg',
+  },
+  {
+    id: '013',
+    brand: 'Rue De Verneul',
+    name: 'Bag',
+    image: 'https://www.net-a-porter.com/variants/images/46376663162985780/ou/w2000_q60.jpg',
+  },
+  {
+    id: '014',
+    brand: 'Pauline Dujancourt',
+    name: 'Bag',
+    image: 'https://www.net-a-porter.com/variants/images/46376663163042934/ou/w2000_q60.jpg',
+  },
+  {
+    id: '015',
+    brand: 'Simone Rocha',
+    name: 'Bag',
+    image: 'https://www.net-a-porter.com/variants/images/46376663163027643/ou/w2000_q60.jpg',
   },
 ];
 
